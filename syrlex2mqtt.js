ef5113eeb29ac964ec280f8e0a5615f1562911b6
Richard Schaller <richard.schaller@gmx.de>
2023-10-31 00:43:08 +0100

    Refactoring mqtt discovery message generation

--- syrlex2mqtt.js
+++ syrlex2mqtt.js
@@ -8,7 +8,6 @@ const brokerUrl = process.env.MQTT_SERVER;
 const username = process.env.MQTT_USER;
 const password = process.env.MQTT_PASSWORD;
 
-
 function formatTimestamp(timestamp) {
   const date = new Date(timestamp*1000);
   const year = date.getFullYear();
@@ -24,121 +23,98 @@ function formatTimestamp(timestamp) {
   return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}${offsetStr}`;
 }
 
-async function sendMQTTSensorDiscoveryMessage(client, model, snr, sw_version, url, sensorname, humanreadable_name, device_class, unit_of_measurement, icon = 'None') {
-  var identifier = (model + snr).toLowerCase();
-  var topic = 'homeassistant/sensor/syr_watersoftening/' + identifier + '_' + sensorname + '/config';
+function generateAvailability(identifier) {
   var availability_topic = 'syr/' + identifier + '/availability';
+  var availability = [
+    {topic: 'syr/bridge/state'},
+    {topic: availability_topic}
+  ];
+  return availability;
+}
+
+function generateMQTTDevice(model, snr, sw_version, url) {
+  var mqttDevice = {
+    identifiers: [ snr ],
+    manufacturer: "Syr",
+    name: model,
+    model: model,
+    sw_version: sw_version,
+    configuration_url: url,
+
+    identifier() {
+      return (this.model + this.identifiers[0]).toLowerCase();
+    }
+  };
+
+  return mqttDevice;
+}
+
+async function sendMQTTSensorDiscoveryMessage(client, mqttDevice, sensorname, humanreadable_name, device_class, unit_of_measurement, icon = 'mdi:water') {
+  var topic = 'homeassistant/sensor/syr_watersoftening/' + mqttDevice.identifier() + '_' + sensorname + '/config';
   var payload = {
         name: humanreadable_name,
         device_class: device_class,
         unit_of_measurement: unit_of_measurement,
         icon: icon,
-        state_topic: 'syr/' + identifier + '/state',
-        availability: [
-          {topic: 'syr/bridge/state'},
-          {topic: availability_topic}
-        ],
+        state_topic: 'syr/' + mqttDevice.identifier() + '/state',
+        availability: generateAvailability(mqttDevice.identifier()),
         value_template: '{{ value_json.'+ sensorname +'}}',
-        unique_id: snr + "_" + sensorname,
-        device: {
-          identifiers: [ snr ],
-          manufacturer: "Syr",
-          name: model,
-          model: model,
-          sw_version: sw_version,
-          configuration_url: url 
-        }
+        unique_id: mqttDevice.identifier() + "_" + sensorname,
+        device: mqttDevice
       }
   await client.publish(topic, JSON.stringify(payload))
 }
 
-async function sendMQTTBinarySensorDiscoveryMessage(client, model, snr, sw_version, url, sensorname, humanreadable_name, device_class) {
-  var identifier = (model + snr).toLowerCase();
-  var topic = 'homeassistant/binary_sensor/syr_watersoftening/' + identifier + '_' + sensorname + '/config';
-  var availability_topic = 'syr/' + identifier + '/availability';
+async function sendMQTTBinarySensorDiscoveryMessage(client, mqttDevice, sensorname, humanreadable_name, device_class) {
+  var topic = 'homeassistant/binary_sensor/syr_watersoftening/' + mqttDevice.identifier() + '_' + sensorname + '/config';
   var payload = {
         name: humanreadable_name,
         device_class: device_class,
-        state_topic: 'syr/' + identifier + '/state',
-        availability: [
-          {topic: 'syr/bridge/state'},
-          {topic: availability_topic}
-        ],
+        state_topic: 'syr/' + mqttDevice.identifier() + '/state',
+        availability: generateAvailability(mqttDevice.identifier()),
         value_template: '{{ value_json.'+ sensorname +'}}',
-        unique_id: snr + "_" + sensorname,
-        device: {
-          identifiers: [ snr ],
-          manufacturer: "Syr",
-          name: model,
-          model: model,
-          sw_version: sw_version,
-          configuration_url: url 
-        }
+        unique_id: mqttDevice.identifier() + "_" + sensorname,
+        device: mqttDevice
       }
   await client.publish(topic, JSON.stringify(payload))
 }
 
 
-async function sendMQTTNumberDiscoveryMessage(client, model, snr, sw_version, url, numbername, humanreadable_name, device_class, unit_of_measurement, minimum, maximum, icon = 'None') {
-  var identifier = (model + snr).toLowerCase();
-  var topic = 'homeassistant/number/syr_watersoftening/' + identifier + '_' + numbername + '/config';
-  var availability_topic = 'syr/' + identifier + '/availability';
+async function sendMQTTNumberDiscoveryMessage(client, mqttDevice, numbername, humanreadable_name, device_class, unit_of_measurement, minimum, maximum, icon = 'mdi:water') {
+  var topic = 'homeassistant/number/syr_watersoftening/' + mqttDevice.identifier() + '_' + numbername + '/config';
   var payload = {
         name: humanreadable_name,
         device_class: device_class,
         unit_of_measurement: unit_of_measurement,
         icon: icon,
-        state_topic: 'syr/' + identifier + '/state',
-        command_topic: 'syr/' + identifier + '/set_' + numbername,
-        availability: [
-          {topic: 'syr/bridge/state'},
-          {topic: availability_topic}
-        ],
+        state_topic: 'syr/' + mqttDevice.identifier() + '/state',
+        command_topic: 'syr/' + mqttDevice.identifier() + '/set_' + numbername,
+        availability: generateAvailability(mqttDevice.identifier()),
         value_template: '{{ value_json.'+ numbername +'}}',
-        unique_id: snr + "_" + numbername,
+        unique_id: mqttDevice.identifier() + "_" + numbername,
         min: minimum,
         max: maximum,
-        device: {
-          identifiers: [ snr ],
-          manufacturer: "Syr",
-          name: model,
-          model: model,
-          sw_version: sw_version,
-          configuration_url: url 
-        }
+        device: mqttDevice
       }
   await client.publish(topic, JSON.stringify(payload))
 }
 
 
-async function sendMQTTButtonDiscoveryMessage(client, model, snr, sw_version, url, buttonname, humanreadable_name) {
-  var identifier = (model + snr).toLowerCase();
-  var topic = 'homeassistant/button/syr_watersoftening/' + identifier + '_' + buttonname + '/config';
-  var availability_topic = 'syr/' + identifier + '/availability';
+async function sendMQTTButtonDiscoveryMessage(client, mqttDevice, buttonname, humanreadable_name) {
+  var topic = 'homeassistant/button/syr_watersoftening/' + mqttDevice.identifier() + '_' + buttonname + '/config';
   var payload = {
         name: humanreadable_name,
-        command_topic: 'syr/' + identifier + '/set_' + buttonname,
-        availability: [
-          {topic: 'syr/bridge/state'},
-          {topic: availability_topic}
-        ],
-        unique_id: snr + "_" + buttonname,
-        device: {
-          identifiers: [ snr ],
-          manufacturer: "Syr",
-          name: model,
-          model: model,
-          sw_version: sw_version,
-          configuration_url: url 
-        }
+        command_topic: 'syr/' + mqttDevice.identifier() + '/set_' + buttonname,
+        availability: generateAvailability(mqttDevice.identifier()),
+        unique_id: mqttDevice.identifier() + "_" + buttonname,
+        device: mqttDevice
       }
   await client.publish(topic, JSON.stringify(payload))
 }
 
 
-async function sendMQTTAvailabilityMessage(client, model, snr) {
-  var identifier = (model + snr).toLowerCase();
-  var availability_topic = 'syr/' + identifier + '/availability';
+async function sendMQTTAvailabilityMessage(client, mqttDevice) {
+  var availability_topic = 'syr/' + mqttDevice.identifier() + '/availability';
 
   await client.publish(availability_topic, 'online', {retain: true})
 }
@@ -172,21 +148,24 @@ const handleConnect = async () => {
   client.subscribe('syr/#')
   client.subscribe('homeassistant/status')
 
-  await sendMQTTSensorDiscoveryMessage(client, model, snr, sw_version, url, 'current_water_flow', 'Current Water Flow', null, 'l/min', 'mdi:water');
-  await sendMQTTSensorDiscoveryMessage(client, model, snr, sw_version, url, 'salt_remaining', 'Salt Remaining', null, 'weeks', 'mdi:cup');
-  await sendMQTTSensorDiscoveryMessage(client, model, snr, sw_version, url, 'remaining_resin_capacity', 'Remaining Resin Capacity', null, '%', 'mdi:water-percent');
-  await sendMQTTSensorDiscoveryMessage(client, model, snr, sw_version, url, 'remaining_water_capacity', 'Remaining Water Capacity', 'water', 'L');
-  await sendMQTTSensorDiscoveryMessage(client, model, snr, sw_version, url, 'total_water_consumption', 'Total Water Consumption', 'water', 'L');
-  await sendMQTTSensorDiscoveryMessage(client, model, snr, sw_version, url, 'number_of_regenerations', 'Number of Regenerations', null, null, 'mdi:counter');
-  await sendMQTTSensorDiscoveryMessage(client, model, snr, sw_version, url, 'last_regeneration', 'Last Regeneration', 'timestamp', null);
-  await sendMQTTBinarySensorDiscoveryMessage(client, model, snr, sw_version, url, 'regeneration_running', 'Regeneration Running', 'running');
-  await sendMQTTButtonDiscoveryMessage(client, model, snr, sw_version, url, 'start_regeneration', 'Start Regeneration');  
-  await sendMQTTSensorDiscoveryMessage(client, model, snr, sw_version, url, 'status_message', 'Status Message', null, null, 'mdi:message-text');
-  
-  await sendMQTTNumberDiscoveryMessage(client, model, snr, sw_version, url, 'salt_in_stock', 'Salt in Stock', 'weight', 'kg', 0, 25, 'mdi:cup');
-  await sendMQTTNumberDiscoveryMessage(client, model, snr, sw_version, url, 'regeneration_interval', 'Regeneration Interval', null, 'days', 1, 10);
+  var mqttDevice = generateMQTTDevice(model, snr, sw_version, url);
 
-  await sendMQTTAvailabilityMessage(client, model, snr);
+  await sendMQTTSensorDiscoveryMessage(client, mqttDevice, 'current_water_flow', 'Current Water Flow', null, 'l/min', 'mdi:water');
+  await sendMQTTSensorDiscoveryMessage(client, mqttDevice, 'salt_remaining', 'Salt Remaining', null, 'weeks', 'mdi:cup');
+  await sendMQTTSensorDiscoveryMessage(client, mqttDevice, 'remaining_resin_capacity', 'Remaining Resin Capacity', null, '%', 'mdi:water-percent');
+  await sendMQTTSensorDiscoveryMessage(client, mqttDevice, 'remaining_water_capacity', 'Remaining Water Capacity', 'water', 'L', 'mdi:water');
+  await sendMQTTSensorDiscoveryMessage(client, mqttDevice, 'total_water_consumption', 'Total Water Consumption', 'water', 'L', 'mdi:water');
+  await sendMQTTSensorDiscoveryMessage(client, mqttDevice, 'number_of_regenerations', 'Number of Regenerations', null, null, 'mdi:counter');
+  await sendMQTTSensorDiscoveryMessage(client, mqttDevice, 'last_regeneration', 'Last Regeneration', 'timestamp', null, 'mdi:clock-time-four-outline');
+  await sendMQTTSensorDiscoveryMessage(client, mqttDevice, 'status_message', 'Status Message', null, null, 'mdi:message-text');
+  await sendMQTTBinarySensorDiscoveryMessage(client, mqttDevice, 'regeneration_running', 'Regeneration Running', 'running');
+
+  await sendMQTTButtonDiscoveryMessage(client, mqttDevice, 'start_regeneration', 'Start Regeneration');
+  
+  await sendMQTTNumberDiscoveryMessage(client, mqttDevice, 'salt_in_stock', 'Salt in Stock', 'weight', 'kg', 0, 25, 'mdi:cup');
+  await sendMQTTNumberDiscoveryMessage(client, mqttDevice, 'regeneration_interval', 'Regeneration Interval', null, 'days', 1, 10, 'mdi:calendar-clock');
+
+  await sendMQTTAvailabilityMessage(client, mqttDevice);
   console.log('Connected');
 }
 
